13dc15826192756c050d0b31353a6ab6116ef3e6
lzagddsg <111216501+lzagddsg@users.noreply.github.com>
2023-02-08 11:10:15 +0800

    添加了主文件的1.0版本
    添加了主文件的1.0版本

---
+++ Cao++.h
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#define 整数 int
+#define 主程序 main
+#define 双精度 double
+#define 布尔 bool
+#define 浮点 float
+#define 包含 include
+#define 定义 define
+#define 打印 printf
+#define 长 long
+#define 可长了 long long
+#define 返回 return
+#define 使用 using
+#define 命名空间 namespace
